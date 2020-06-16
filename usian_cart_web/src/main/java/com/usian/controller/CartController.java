4ee867b062acd64ff6be5a9e628beeb3432b5aad
kxxl1234 <209876080@qq.com>
2020-06-16 23:00:40 +0800

    提交usian_parent导github11

--- usian_cart_web/src/main/java/com/usian/controller/CartController.java
+++ usian_cart_web/src/main/java/com/usian/controller/CartController.java
@@ -100,6 +100,13 @@ public class CartController {
 
             }else {
                 //登陆
+                Map<String, TbItem> cart = getCartFormRedis(userId);
+                Set<String> keySet = cart.keySet();
+                for (String itemId : keySet){
+                    TbItem tbItem = cart.get(itemId);
+                    tbItemList.add(tbItem);
+                }
+
             }
             return Result.ok(tbItemList);
         }catch (Exception e){
@@ -113,17 +120,31 @@ public class CartController {
     public Result updateItemNum(String userId,Long itemId,Integer num,
                                 HttpServletRequest request,HttpServletResponse response){
         try {
-            //1、获得cookie中的购物车
-            Map<String, TbItem> cart =  getCartFormCookie(request);
-            //2、修改购物车中的商品
-            TbItem tbItem = cart.get(itemId.toString());
-           if (tbItem!=null){
-               tbItem.setNum(num);
-           }
-            cart.put(itemId.toString(),tbItem);
-            //3、把购物车写到cookie
-            addClientCookie(cart,request,response);
-            //登录
+            if (StringUtils.isBlank(userId)){
+               //未登录
+                //1、获得cookie中的购物车
+                Map<String, TbItem> cart =  getCartFormCookie(request);
+                //2、修改购物车中的商品
+                TbItem tbItem = cart.get(itemId.toString());
+                if (tbItem!=null){
+                    tbItem.setNum(num);
+                }
+                cart.put(itemId.toString(),tbItem);
+                //3、把购物车写到cookie
+                addClientCookie(cart,request,response);
+            }else {
+                //登录
+                //1、获得cookie中的购物车
+                Map<String, TbItem> cart = getCartFormRedis(userId);
+
+                //2、修改购物车中的商品
+                TbItem tbItem = cart.get(itemId.toString());
+                tbItem.setNum(num);
+                cart.put(itemId.toString(),tbItem);
+
+                //3、把购物车写到redis
+                addCartToRedis(cart,userId);
+            }
             return Result.ok();
         }catch (Exception e){
             e.printStackTrace();
@@ -138,12 +159,23 @@ public class CartController {
         try {
             if (StringUtils.isBlank(userId)) {
                 //在用户未登录的状态下
+                //1.获得cookie的购物车
                 Map<String,TbItem> cart = getCartFormCookie(request);
+                //2.删除购物车中的商品
                 cart.remove(itemId.toString());
+                //3.把购物车写到cookie
                 addClientCookie(cart,request,response);
-
             } else {
                 // 在用户已登录的状态
+                //1.获得redis的购物车
+                Map<String, TbItem> cart = getCartFormRedis(userId);
+
+                //2.删除购物车中的商品
+                cart.remove(itemId.toString());
+
+                //3.把购物车写到redis中
+                addCartToRedis(cart,userId);
+
             }
             return Result.ok();
         } catch (Exception e) {
@@ -159,15 +191,15 @@ public class CartController {
     }
 
     private void addItemToCart(Map<String, TbItem> cart, Long itemId, Integer num) {
-       TbItem tbItem = cart.get(itemId.toString());
-       if (tbItem!=null){
-           //购物已存在该商品：数量num
-           tbItem.setNum(tbItem.getNum()+num);
-       }else {
-           tbItem = itemServiceFeign.selectItemInfo(itemId);
-           tbItem.setNum(num);
-       }
-       cart.put(itemId.toString(),tbItem);
+        TbItem tbItem = cart.get(itemId.toString());
+        if (tbItem!=null){
+            //购物已存在该商品：数量num
+            tbItem.setNum(tbItem.getNum()+num);
+        }else {
+            tbItem = itemServiceFeign.selectItemInfo(itemId);
+            tbItem.setNum(num);
+        }
+        cart.put(itemId.toString(),tbItem);
     }
 
     private Map<String, TbItem> getCartFormCookie(HttpServletRequest request) {
---
+++ usian_order_web/src/main/java/com/usian/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.usian.config;
+
+import com.usian.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration registration = registry.addInterceptor(loginInterceptor);
+        registration.addPathPatterns("/frontend/order/**");
+    }
+}
